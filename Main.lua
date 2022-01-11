1a386354f74f5b45dc545359f313d20362ec7ed8
x2Storm <93361617+IEEAS@users.noreply.github.com>
2022-01-11 19:23:07 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,5 +1,7 @@
 if game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then
     if is_synapse_function then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/IEEAS/PnegusHub/main/BloxFruitSynapse.lua"))()
+    else
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/IEEAS/PnegusHub/main/BloxFruitSynapse.lua"))()
     end
 end
