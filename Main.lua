b52fe22227305bec59fd3d4bd034b805eafebdcf
x2Storm <93361617+IEEAS@users.noreply.github.com>
2022-01-24 15:55:43 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,7 +1,6 @@
 if game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then
-    if is_synapse_function then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/IEEAS/PnegusHub/main/BloxFruitSynapse.lua"))()
-    else
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/IEEAS/PnegusHub/main/BloxFruitFreeExploit.lua"))()
-    end
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/IEEAS/PnegusHub/main/PrimeHub.lua"))()
+end
+if game.PlaceId == 8357510970 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/IEEAS/PnegusHub/main/PrimePunch.lua"))()
 end
